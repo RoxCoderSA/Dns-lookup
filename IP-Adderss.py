3351482b50fd9b957d473ceb83736db75b453914
Roxcoder <rexcode@outlook.de>
2021-12-09 11:24:07 +0300

    modifing python file and adding new capture

--- IP-Adderss.py
+++ IP-Adderss.py
@@ -9,7 +9,7 @@ print("""
          888   8888888P"                      d88P  888 d88" 888 d88" 888 d8P  Y8b 888P"   88K      88K      
          888   888             888888        d88P   888 888  888 888  888 88888888 888     "Y8888b. "Y8888b. 
          888   888                          d8888888888 Y88b 888 Y88b 888 Y8b.     888          X88      X88 
-       8888888 888                         d88P     888  "Y88888  "Y88888  "Y8888  888      88888P'  88888P'  ® Roxcoder                                                                                          
+       8888888 888                         d88P     888  "Y88888  "Y88888  "Y8888  888      88888P'  88888P'   @Roxcoder                                                                                          
 """)
 
 #Create varble call parser and adding description of script
